9d89c75fe1f3e47c220fcfca4462a728c5a3ebbe
concept-x <emilie_elliotthotmail.com>
2017-10-21 17:52:27 -0500

    modal window shows up & closes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,26 +14,22 @@
                 templateUrl: '/templates/modal.html'
               })
   */
-  //adding above made modal.html show up; home (showing chat rooms) = gone
+  //adding above made modal.html show up, replacing home.html; still no modal window
         $stateProvider
             .state('home', {
                 url: '/',
                 controller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
             })
-            
-            $stateProvider
-              .state('modal', {
-                url: '/',
-                controller: 'ModalCtrl as modal',
-                templateUrl: '/templates/modal.html'
-              });
+
+          
     }
 
      angular
          .module('blocChatEe', ['ui.router', 'ui.bootstrap', 'firebase'])
          .config(config);
- })();
+ })();//adding dbl parentheses here didn't make a difference w/functionality
+
 //above: my file; below: found on interwebs
 /*
 var app = angular.module("MyApp", ["ui.bootstrap.modal"]);
