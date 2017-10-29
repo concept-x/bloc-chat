9456ba227894a48b794aa27052b12940ead9c2a8
concept-x <emilie_elliotthotmail.com>
2017-10-28 20:17:00 -0500

    ckpoint 4:messages not showing

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,7 @@
       this.rooms = Room.all;
       $scope.greeting = "Bloc Chat Home Page!!!";
       //everything made in this controller must be inside this function, or else no accessibility
+      $scope.activeRoom = "This is the active room.";//Active room header. Works, yay!!
     }//$scope = obj that lives in controller that we have access to in template.
 
     angular
